b27bba3b58e685e46c3469eef6dbfa4b4384a4ba
Erin Cunningham <45298977+Barbara-Gordon@users.noreply.github.com>
2019-05-31 18:39:27 -0400

    donezo

--- static/js/app.js
+++ static/js/app.js
@@ -10,18 +10,37 @@ submit.on("click", function() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
+
     // Get the value property of the input element
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
     console.log(inputValue)
 
-    //select the table
-    var ufoTable = d3.select("#ufo-table");
-   
+    //select the table    
+    var ufoTable = d3.select("#ufo-table>tbody");
     
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
     console.log(filteredData)
 
-    filteredData.foreach(sighting => )
+    filteredData.forEach((sighting) => {
+    
+      //Add a new row
+      var row = ufoTable.append("tr");
+      //Add values to cells in row
+      //Date
+      row.append("td").text(sighting.datetime);
+      //City
+      row.append("td").text(sighting.city);
+      //State
+      row.append("td").text(sighting.state);      
+      //Country
+      row.append("td").text(sighting.country);
+      //Shape
+      row.append("td").text(sighting.shape);
+      //Duration
+      row.append("td").text(sighting.durationMinutes);
+      //Comments
+      row.append("td").text(sighting.comments);      
+
+    });
   });
-  
